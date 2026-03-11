f5927811b2bf634df7f24bca5f5c412ee5fd4e09
bw0038 <i.lombardo@alumnos.upm.es>
2026-03-11 23:42:09 +0100

    Tests terminados, haciendo Pruebas

--- src/main/java/evaluador/Pruebas.java
+++ src/main/java/evaluador/Pruebas.java
@@ -26,31 +26,35 @@ public class Pruebas {
         System.out.println("Convocatorias en ED: " + lista1.numConvocatorias("ED"));
         System.out.println("Convocatorias en Algebra: " + lista1.numConvocatorias("Algebra"));
         System.out.println("Convocatorias en Fundamentos de Programacion: " + lista1.numConvocatorias("Fundamentos de Programacion"));
+
         Alumno a1 = new Alumno("Felipe Garcia Gomez", 1253);
         Alumno a2 = new Alumno("Alicia Blazquez Martin", 5622);
         for(int i=0; i < lista1.getNumElementos(); i++){
             a1.nuevaEvaluacion(lista1.getElemento(i));
         }
+
         Evaluacion ev5 = new Evaluacion("ED", "Junio 20", 3);
         a1.nuevaEvaluacion(ev5);
+
         System.out.println("---ASIGNATURAS APROBADAS POR "+ a1.getNombre()+ "---");
-        Lista aprobadas1 = a1.asignaturasAprobadas();
-        if(!aprobadas1.vacia()){
-            for(int i = 0; i < aprobadas1.getNumElementos(); i++){
-                aprobadas1.getElemento(i).mostrar();
-            }
-        }
+        mostrar(a1.asignaturasAprobadas());
         System.out.println();
         System.out.println("---ASIGNATURAS APROBADAS POR "+ a2.getNombre()+ "---");
-        Lista aprobadas2 = a2.asignaturasAprobadas();
-        if(!aprobadas2.vacia()){
-            for(int i = 0; i < aprobadas1.getNumElementos(); i++){
-                aprobadas1.getElemento(i).mostrar();
-            }
-        }
+        mostrar(a2.asignaturasAprobadas());
 
         a1.mostrar();
         a2.mostrar();
+    }
+
+    public static boolean mostrar(Lista lista){
+        boolean resultado = false;
+        if(!lista.vacia()){
+            Iterador it = lista.getIterador();
+            while(it.hasNext()) {
+                it.next().mostrar();
+            }
+        }
+        return resultado;
     }
 }
     
